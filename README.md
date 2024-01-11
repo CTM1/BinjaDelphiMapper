253e38adf945db7a334d2f1ea7a578e4c97273c9
CTM1 <45026430+CTM1@users.noreply.github.com>
2024-01-11 14:18:28 +0100

    feat(plugin): V1

--- README.md
+++ README.md
@@ -1,3 +1,37 @@
-# DelphiMapper
+# Delphi Mapper (v0.1)
+Author: **Claudio Teixeira**
 
-A Binary Ninja plugin to rename Delphi functions from a .map file taken from the IDR disassembler.
+_A plugin that renames Delphi functions according to a .map file produced by IDR._
+
+## Description:
+
+Parses a .map file and replaces the names of functions at their corresponding adresses.
+
+
+## Installation Instructions
+
+### Darwin
+
+
+
+### Windows
+
+
+
+### Linux
+
+
+
+## Minimum Version
+
+This plugin requires the following minimum version of Binary Ninja:
+
+* 1200
+
+
+## License
+
+This plugin is released under a MIT license.
+## Metadata Version
+
+2
---
+++ __init__.py
@@ -0,0 +1,41 @@
+import binaryninja
+from binaryninja import PluginCommand, BinaryView, interaction
+
+system_libs = ['kernel32', 'system.', 'Windows.']
+
+def rename_functions(bv: BinaryView, map_file_path: str, create: bool):
+    with open(map_file_path, 'r') as file:
+        for line in file:
+            if not any(prefix in line for prefix in system_libs) and line.count('_') >= 1:
+                try:
+                    parts = line.split()[1].split('_')
+                    new_name = parts[0]
+                    addr = int(parts[1], 16)
+
+                    fn = bv.get_function_at(addr)
+                    if fn:
+                        fn.name = new_name
+                    else:
+                        if (create):
+                            fn = bv.add_function(addr, bv.platform, True)
+                            fn.name = new_name
+                        else:
+                            print(f"function {new_name} at {hex(addr)} not found, not creating")
+                except:
+                    continue  # Ignore lines that don't match the expected format
+
+def map_importer_plugin(bv: BinaryView, create: bool):
+    map_file_path = interaction.get_open_filename_input("Select a map file", "*.map")
+    if map_file_path:
+        rename_functions(bv, map_file_path, create)
+    else:
+        print("No file selected.")
+
+def rename_fn(bv: BinaryView):
+    map_importer_plugin(bv, False)
+
+def create_and_rename_fn(bv: BinaryView):
+    map_importer_plugin(bv, True)
+
+PluginCommand.register("Delphi Mapper\\0. Import .map, create and rename functions", "Imports names from a .map file and create function objects where possible", create_and_rename_fn)
+PluginCommand.register("Delphi Mapper\\1. Import .map and only rename existing functions", "Imports names from a .map file and rename existing functions", rename_fn)
